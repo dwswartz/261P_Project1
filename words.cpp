1bf76d157dd075f84d3b389e210c1800450173fe
dwswartz <88454256+dwswartz@users.noreply.github.com>
2022-02-20 20:30:39 -0800

    Update words.cpp

--- words.cpp
+++ words.cpp
@@ -52,98 +52,18 @@ namespace words {
     size_t k
   ) noexcept {
 
-
+    std::vector<words::word_frequency> tempdist;
     std::vector<words::word_frequency> topk;
+    tempdist = dist;
 
-    for (int i = 0; i < k; i++) {
-      topk.push_back(dist.at(i));
+    std::sort(tempdist.rbegin(), tempdist.rend(),         
+    [&](const words::word_frequency& word1, const words::word_frequency& word2)
+    { return (word1.count() < word2.count());} );     // sort the vector of word_frequency by count 
+                                                      // and reverse the iterators to get descending order
+
+    for (int i = 0; i < k; i++){    
+      topk.push_back(tempdist.at(i));                 // get top k elements 
     }
-
-    // std::vector<words::word_frequency>::iterator minit; 
-
-    // for (auto x :dist){
-    //   for (minit = topk.begin(); minit != topk.end();){
-    //     if (minit->count() < x.count()){
-    //       *minit = x;
-    //     }
-    //     ++minit;
-    //   }
-    // }
-    std::vector<words::word_frequency>::iterator minfreq = topk.begin();
-    std::vector<words::word_frequency>::iterator tmp;
-
-    for (int i = 0; i < dist.size(); i++) {
-      // minfreq = std::min_element(topk.begin(), topk.end(),
-      //   [&] (const words::word_frequency& left, const words::word_frequency& right) 
-      //   {return left.count() < right.count();}
-      //   );
-      if (dist.at(i).count() > minfreq->count()) {
-        *minfreq = dist.at(i);
-        for (tmp = topk.begin(); tmp != topk.end();) {
-          if (minfreq->count() > tmp->count()){
-            minfreq = tmp;
-          }
-          tmp++;
-        }
-        // for (int j = 0; j < topk.size(); j++) {
-        //   if (minfreq->count() > topk.at(j).count()) {
-        //     *minfreq = topk.at(j);
-        //   }
-        // }
-      }
-    }
-
-    // std::vector<std::pair<std::string,int>> topk2; 
-
-    // std::vector<std::pair<std::string,int>> dist2;
-
-    // for (int i = 0; i < dist.size(); i++){
-
-    //   dist2[i].first = dist[i].word();
-    //   dist2[i].second = dist[i].count();
-
-    // }
-
-    // sort(dist2.begin(), dist2.end(), [&](std::pair<std::string, int> &left, std::pair<std::string, int> &right){ 
-    //   return left.second> right.second;
-    // } );
-
-
-    // for (int i = 0; i < k; i++) {
-    //   topk2.push_back(dist2[i]);
-    // }
-
-    // for (auto x : topk2){
-    //   topk.push_back(words::word_frequency(x.first, x.second));
-    // }
-
-    // for (int i = 0; i < k; i++) {
-    //   topk.push_back(dist.at(i));      
-    // }
-    
-    // int min_ele; 
-
-    // for (auto x : dist){
-    //   min_ele = std::min_element(topk.begin(), topk.end(),  )
-    // }
-
-    // int i=0;
-    // for (auto x : dist) {
-    //   if (i++ < k) {
-    //     topk.push_back(x);
-    //   }
-    //   else if (x.count() > topk.begin()->count()) {
-    //     topk.erase(topk.begin());
-    //     topk.push_back(x);
-    //   }
-    // }
-    
-
-    
-    // TODO: Write a definition for this function that actually
-    // works. That will include deleting the current return statement,
-    // which is just a placeholder to allow the code to compile. When
-    // you are done, delete this TODO comment.
 
     return topk;
   }
@@ -154,11 +74,6 @@ namespace words {
     const word_vector& document
   ) const noexcept {
 
-    // TODO: Write a definition for this function that actually
-    // works. That will include deleting the current return statement,
-    // which is just a placeholder to allow the code to compile. When
-    // you are done, delete this TODO comment.
-
     std::vector<std::string> stopWords;    // Initialize vector of stop words
     stopWords = stop_words;              // Copy stop words into vector
     std::vector<words::word_frequency> frequencyDistribution;     // Initialize vector of word frequencies
@@ -167,15 +82,8 @@ namespace words {
 
     for (int i = 0; i < document.size(); i++) {
       if (std::find(stopWords.begin(), stopWords.end(), document.at(i)) == stopWords.end()) {     // Check if word is stop word
-
-        // bool isEqual(const words::word_frequency& word) {
-        //   return word.word() == dictionary.at(i);
-        // }
-        // it = std::find_if(frequencyDistribution.begin(), frequencyDistribution.end(), isEqual());
-
-       //it = find(frequencyDistribution.begin()->word(), frequencyDistribution.end()->word(), document.at(i)); // Find index of word
        it = std::find_if(frequencyDistribution.begin(), frequencyDistribution.end(), 
-        [&](const words::word_frequency& word) {return word.word() == document.at(i);});
+        [&](const words::word_frequency& word) {return word.word() == document.at(i);});    // iterator to find if word is in the distribution vector
         if (it != frequencyDistribution.end()) {        // Word exists in frequency table
           it->increment_count();          // Increment frequency of second element of pair 
         }   
@@ -192,11 +100,6 @@ namespace words {
     const word_vector& stop_words,
     const word_vector& document
   ) const noexcept {
-
-    // TODO: Write a definition for this function that actually
-    // works. That will include deleting the current return statement,
-    // which is just a placeholder to allow the code to compile. When
-    // you are done, delete this TODO comment.
 
     std::list<std::string> stopWords;       // Initialize linked list of stop words
     std::list<words::word_frequency> frequencyDistribution;     // Initialize linked list of word frequencies
@@ -224,7 +127,7 @@ namespace words {
     std::vector<words::word_frequency> dists;     // Initialize vector of word frequencies
 
     for (words::word_frequency &wf: frequencyDistribution) {
-      dists.push_back(wf);
+      dists.push_back(wf);              // convert list to vector
     }
 
     return dists;
@@ -236,18 +139,12 @@ namespace words {
     const word_vector& document
   ) const noexcept {
 
-    // TODO: Write a definition for this function that actually
-    // works. That will include deleting the current return statement,
-    // which is just a placeholder to allow the code to compile. When
-    // you are done, delete this TODO comment.
-
     std::unordered_set<std::string> stopWords;      // Initialize hash table of stop words
     for (int i = 0; i < stop_words.size(); i++) {
       stopWords.insert(stop_words.at(i));       // Populate hash table with stop words 
     }
 
     std::unordered_map<std::string, int> frequencyDistribution;     // Initalize hash table of word frequencies
-    //std::unordered_set<words::word_frequency> frequencyDistribution; 
 
     for (int i = 0; i < document.size(); i++) {       // Iterate through document 
       if (stopWords.find(document.at(i)) == stopWords.end()) {      // Check if word is stop word 
@@ -275,11 +172,6 @@ namespace words {
     const word_vector& document
   ) const noexcept {
 
-    // TODO: Write a definition for this function that actually
-    // works. That will include deleting the current return statement,
-    // which is just a placeholder to allow the code to compile. When
-    // you are done, delete this TODO comment.
-    
     std::set<std::string> stopWords; // initialize stop words
 
     for (int i = 0; i < stop_words.size(); i++) {
